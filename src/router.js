243776326b24233a0186f6c77c7b6e941e1cd137
David Gonzalez <davidglezsk8@gmail.com>
2019-05-25 19:23:01 +0200

    ya me cansé de tanta poesía, pues prefiero picar aunque sea una porquería.

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HomeLayout from './layouts/Home.vue'
+import Cinema from './views/Cinema'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: HomeLayout
+    },
+    {
+      path: '/cinema/:port/:user',
+      name: 'cinema',
+      component: Cinema
     }
     // {
     //   path: '/about',
--- src/views/Cinema.vue
+++ src/views/Cinema.vue
@@ -1,13 +1,73 @@
 <template>
-<dialog></dialog>
+  <div class="columns">
+    <div id="video" class="column">
+      <video class="stream" style="width: 100%" controls
+      >
+        <source :src="streamSrc">
+      </video>
+    </div>
+    <div id="chat" class="column is-one-fifth">
+        <div class="chat">
+
+        </div>
+        <div class="control">
+          <input class="input" type="text" placeholder="Chat" required>
+        </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Cinema'
+  name: 'Cinema',
+  computed: {
+    streamSrc () {
+      return 'http://sharefilm.ibon.dev:' + this.$route.params.port
+    }
+  }
 }
 </script>
 
 <style scoped>
+#chat {
+  position: absolute;
+  right: 0;
+  background: #1c212e;
+  height: 101%;
+}
 
+.chat {
+  height: 94%;
+}
+
+#video {
+  position: absolute;
+  left: 0;
+  width: 80%;
+  padding: 0;
+}
+
+.stream {
+  margin-top: 8%;
+}
+
+.control {
+  position: fixed;
+  bottom: 0;
+  background: linear-gradient(135deg, #8ebcee, #388fff);
+  padding: 3px;
+  width: 18%;
+  margin-bottom: 10px;
+}
+
+.input {
+  border-radius: 0px;
+  background: #1c212e;
+  border: 1px solid transparent;
+  color: #949cb0;
+}
+
+.input::placeholder {
+  color: #949cb0;
+}
 </style>
